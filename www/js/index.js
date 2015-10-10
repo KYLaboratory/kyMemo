bdc6e9e4a618ea7cf4a7e87059adecaff3c0e968
Takuya Mizuhara <z8x8c.zeolite.m@gmail.com>
2015-10-10 16:31:09 +0900

    Implement searching tweets

--- www/js/index.js
+++ www/js/index.js
@@ -57,6 +57,91 @@ var app = {
         }
 
         var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
+
+        var consumerKey    = "oDYBiFlyMdlEjGZv6d8DKQ1DF";
+        var consumerSecret = "Q5lXboEPseBpaIKnKkMLNHZmOnEM5wIs755bg0jv0fhUCCGnDE";
+        var accessToken    = "3838308979-RSBMMC6L1HjI23gZ8EXMiMBGiwv99yeMaUok0ef";
+        var tokenSecret    = "88omD7pr8GuTpLlVqhTbsSjXmB5hsLQk0zy0UnrNlDMCu";
+
+        var count = 10; // 表示する件数
+
+        // Twitter APIを使用してTweetを取得する部分
+        // function getTwitter(action, options) {
+        function getTwitter(action, keywords) {
+
+            var accessor = {
+                consumerSecret: consumerSecret,
+                tokenSecret: tokenSecret
+            };
+
+            // 送信するパラメータを連想配列で作成
+            var message = {
+                method: "GET", // リクエストの種類
+                action: action,
+                parameters: {
+                    oauth_version: "1.0",
+                    oauth_signature_method: "HMAC-SHA1",
+                    oauth_consumer_key: consumerKey, // コンシューマーキー
+                    oauth_token: accessToken, // アクセストークン
+                    count: count, // 取得する件数
+                    "q": keywords, // 検索するキーワード
+                    "lang": "ja", // 日本語に設定
+                    "result_type": "recent", // 最新の情報を取得するように設定
+                    callback: "update" // 取得したデータをupdate関数に渡すよう設定
+                }
+            };
+
+            // OAuth認証関係
+            OAuth.setTimestampAndNonce(message);
+            OAuth.SignatureMethod.sign(message, accessor);
+            var url = OAuth.addToURL(message.action, message.parameters);
+
+            // ajaxによる通信
+            $.ajax({
+                type: message.method,
+                url: url, // リクエスト先のURL
+                dataType: "jsonp",
+                jsonp: false,
+                cache: true,
+            });
+        }
+
+        // UIの更新
+        function update(data){ // 引数(data)に取得したデータが入ってくる
+            alert("1");
+            $(".TweetArea").empty(); // 表示エリアを空にする
+            var result = data.statuses; // 取得したデータから、メソッドチェーンで必要なものを取得
+            for( var i = 0; i < result.length; i++ ) {
+                var name = result[i].user.name; // ツイートした人の名前
+                var imgsrc = result[i].user.profile_image_url; // ツイートした人のプロフィール画像
+                var content = result[i].text; // ツイートの内容
+                var updated = result[i].created_at; // ツイートした時間
+                var time = "";
+                alert("test3"); 
+                // Tweet表示エリアに取得したデータを追加していく
+                $(".TweetArea").append('<img src="'+imgsrc+'" />' + '<p>' + name + ' | ' + content + ' | ' + updated + '</p>');
+            }
+        }
+        
+        // idに検索ボタンと指定されている button要素にクリックイベントを設定
+        document.getElementById("searchButton").addEventListener("click", function(){
+            // idにキーワード入力欄と指定されている input要素に入力されている値を取得
+            var keywords = document.getElementById("keywordInput").value;
+            // keywordsに何も入力されていなかった場合の処理
+            if (keywords == "") {
+                alert("何も入力されていないようです。"); // メッセージを表示
+                return; // 処理を中断
+            }
+            else{
+                // alert(keywords + "　が入力されています。");
+            }
+
+            // リクエスト先のURL
+            var url = "https://api.twitter.com/1.1/search/tweets.json";
+
+            // Tweet検索関数の呼び出し
+            getTwitter(url, keywords);
+        });
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {

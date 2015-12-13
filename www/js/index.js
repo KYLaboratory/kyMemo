cdedaff6d6c80aff5c0f053d97aa85300cc2f8e0
Caozy <Koji.Suzuki.cc@gmail.com>
2015-12-14 01:26:27 +0900

    change index.html and index.js

--- www/js/index.js
+++ www/js/index.js
@@ -33,6 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+
         // Define a div tag with id="map_canvas"
         var mapDiv = document.getElementById("map_canvas");
 
@@ -48,6 +49,9 @@ var app = {
 
         // マップのカメラを初期位置に移動したか
         var initializedCameraPosition = false;
+
+        // 表示コンテンツを管理する連想配列
+        var contentsMap = new Object();
 
         // Twitter API利用のための初期化処理
         var consumerKey    = "oDYBiFlyMdlEjGZv6d8DKQ1DF";
@@ -76,14 +80,6 @@ var app = {
         // current GPS coordinates
         //
         var onSuccess = function(position) {
-            // alert('Latitude: '          + position.coords.latitude          + '\n' +
-            //       'Longitude: '         + position.coords.longitude         + '\n' +
-            //       'Altitude: '          + position.coords.altitude          + '\n' +
-            //       'Accuracy: '          + position.coords.accuracy          + '\n' +
-            //       'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-            //       'Heading: '           + position.coords.heading           + '\n' +
-            //       'Speed: '             + position.coords.speed             + '\n' +
-            //       'Timestamp: '         + position.timestamp                + '\n');
             var element = document.getElementById('geolocation');
             element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br>' +
                                 'Longitude: ' + position.coords.longitude     + '<br>' +
@@ -99,11 +95,16 @@ var app = {
                 initializedCameraPosition = true;
             }
 
-            // リクエスト先のURL
-            var url = "https://api.twitter.com/1.1/search/tweets.json";
-
             // Tweet検索関数の呼び出し
-            getTweets(url, position.coords.latitude, position.coords.longitude);
+            getTweets(position.coords.latitude, position.coords.longitude);
+
+            // ぐるなび検索関数の呼び出し
+            getGurunabi(position.coords.latitude, position.coords.longitude);
+
+            // コンテンツを表示する
+            for (key in contentsMap ){
+                addContents(contentsMap[key]);
+            }
         };
 
         // onError Callback receives a PositionError object
@@ -118,33 +119,70 @@ var app = {
         var count = 10; // 表示する件数
 
         // UIの更新
+        function updateGurunabiList(result) {
+            for ( var i in result.rest ){
+                var id = result.rest[i].id;
+
+                // 既に入っているものは無視
+                if(id in contentsMap) {
+                    return;
+                }
+
+                // 新しいものを連想配列に追加する
+                contentsMap[id] = {
+                    id: id,
+                    name: result.rest[i].name, // 店舗名称
+                    icon: 'http://pbs.twimg.com/profile_images/548048770592550913/C0PVdzHi_normal.jpeg', //
+                    text: result.rest[i].url, // サイトURL
+                    time: result.rest[i].update_date, // 情報更新日時,
+                    lat: result.rest[i].latitude,
+                    lng: result.rest[i].longitude
+                };
+            }
+        }
+
+        function getGurunabi(lat, lng) {
+            var url = 'http://api.gnavi.co.jp/RestSearchAPI/20150630/?';
+
+            // 検索するパラメータ
+            var params = {
+                keyid: 'd9e0f9d2f4b8c8f75673a4a43bf09479',
+                format: 'json',
+                latitude: lat,
+                longitude: lng,
+                range: 2,
+                input_coordinates_mode: 1
+            };
+
+            $.getJSON(url, params, function(result){
+                updateGurunabiList(result);
+            });
+        }
+
         function updateTweetList(data){ // 引数(data)に取得したデータが入ってくる
-            $(".TweetList").empty(); // 表示エリアを空にする
             var result = data.statuses; // 取得したデータから、メソッドチェーンで必要なものを取得
             for( var i = 0; i < result.length; i++ ) {
-                var name = result[i].user.name; // ツイートした人の名前
-                var imgsrc = result[i].user.profile_image_url; // ツイートした人のプロフィール画像
-                var content = result[i].text; // ツイートの内容
-                var updated = result[i].created_at; // ツイートした時間
-                var coordinates = result[i].geo.coordinates;
-                var time = "";
-                // Tweet表示エリアに取得したデータを追加していく
-                $(".TweetList").append('<img src="'+imgsrc+'" />' + '<p>' + name + ' | ' + content + ' | ' + updated + '</p>');
-                map.addMarker({
-                    'position': new plugin.google.maps.LatLng(coordinates[0], coordinates[1]),
-                    'title': content,
-                    'icon': {
-                        url: imgsrc
-                    }
-                }, function(marker) {
-                    marker.showInfoWindow();
-                });
+                var id = result[i].id;
+                // 既に入っているものは無視
+                if(id in contentsMap) {
+                    return;
+                }
+
+                // 新しいものを連想配列に追加する
+                contentsMap[id] = {
+                    id: id,
+                    name: result[i].user.name, // ツイートした人の名前
+                    icon: result[i].user.profile_image_url, // ツイートした人のプロフィール画像
+                    text: result[i].text, // ツイートの内容
+                    time: result[i].created_at, // ツイートした時間,
+                    lat: result[i].geo.coordinates[0],
+                    lng: result[i].geo.coordinates[1]
+                };
             }
         }
 
         // Twitter APIを使用してTweetを取得する部分
-        function getTweets(action, latitude, longitude) {
-
+        function getTweets(latitude, longitude) {
             // 初期化していなければ何もしない
             if(bearerToken == null) {
                 return;
@@ -165,46 +203,63 @@ var app = {
                     updateTweetList(reply);
                 }
             );
-
-            // var accessor = {
-            //     consumerSecret: consumerSecret,
-            //     tokenSecret: tokenSecret
-            // };
-
-            // // 送信するパラメータを連想配列で作成
-            // var message = {
-            //     method: "GET", // リクエストの種類
-            //     action: action,
-            //     parameters: {
-            //         oauth_version: "1.0",
-            //         oauth_signature_method: "HMAC-SHA1",
-            //         oauth_consumer_key: consumerKey, // コンシューマーキー
-            //         oauth_token: accessToken, // アクセストークン
-            //         count: count, // 取得する件数
-            //         "q": "geocode:" + latitude + ',' + longitude + ',' + "1km" + " -I'm", // 検索するキーワード
-            //         "lang": "ja", // 日本語に設定
-            //         "result_type": "recent" // 最新の情報を取得するように設定
-            //     }
-            // };
-
-            // // OAuth認証関係
-            // OAuth.setTimestampAndNonce(message);
-            // OAuth.SignatureMethod.sign(message, accessor);
-            // var url = OAuth.addToURL(message.action, message.parameters);
-
-            // // ajaxによる通信
-            // $.ajax({
-            //     type: message.method,
-            //     url: url, // リクエスト先のURL
-            //     cache: true,
-            // }).done(function(data){
-            //     updateTweetList(data);
-            // }).fail(function(xhr, ajaxOptions, thrownError){
-            //     alert(ajaxOptions);
-            //     alert(thrownError);
-            // });
         }
-        
+
+        // 表示コンテンツを追加する（上から追加する）
+        function addContents(contents) {
+
+            // 既に表示しているコンテンツは無視する
+            if($("#" + contents.id).length != 0) {
+                return;
+            }
+
+            var id = contents.id;
+            var lat = contents.lat;
+            var lng = contents.lng;
+            var text = contents.text;
+            var icon = contents.icon;
+            var time = contents.time;
+            
+            // リスト先頭にコンテンツを追加する
+            $(".placeline").prepend('<li class="item" id=' + id + '>' +
+                    '<div class="tweet_body">' +
+                        '<div class="icon_image"><img src="' + icon +'" /></div>' +
+                        '<div class="tweet_content">' +
+                            '<div class="tweet_row">' +
+                                // '<div class="user_name">' +
+                                //     '<span class="full_name">' + contents.name + '</span>' +
+                                //     '<span class="screen_name">' + @foo + '</span>' + 
+                                // '</div>' +
+                                '<div class="time_stamp">' + time + '</div>' +
+                            '</div>' +
+                            '<div class="tweet_text">' +
+                                text + 
+                            '</div>' +
+                        '</div>' +
+                    '</div>' +
+                '</li>');
+
+            // 地図にアイコンを追加する
+            map.addMarker({
+                'position': new plugin.google.maps.LatLng(lat, lng),
+                'title': text,
+                'icon': {
+                    url: icon
+                }
+            }, function(marker) {
+                marker.showInfoWindow();
+            });
+        }
+
+        // 指定されたコンテンツを削除する
+        function removeContents(contents) {
+            // 指定されたIDのリストを削除する
+            $(".TweetList").find("#" + contents.id).remove();
+
+            // 地図のアイコンを削除する
+            // TODO
+        }
+
         // (デバッグ用)idに検索ボタンと指定されている button要素にクリックイベントを設定
         document.getElementById("searchButton").addEventListener("click", function(){
             // idにキーワード入力欄と指定されている input要素に入力されている値を取得
